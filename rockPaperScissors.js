b8309023f874eaefe00b7b5cc8c271f0fcff5a4a
TorchMapCrown <266081775+TorchMapCrown@users.noreply.github.com>
2026-03-27 19:40:06 -0400

    added playRound logic for determining round winner

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,44 +1,93 @@
-const rock = "rock"
-const paper = "paper"
-const scissors = "scissors"
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
 
 function getComputerChoice(min, max) {
     // randomly assigns roll an integer value between 1 and 3
-    let min = Math.ceil(min);
-    let max = Math.floor(max);
-    let roll = Math.floor(Math.random() * (max - min + 1) + min);
-    
-    let result = ""
+    let minCeil = Math.ceil(min);
+    let maxFloor = Math.floor(max);
+    let roll = Math.floor(Math.random() * (maxFloor - minCeil + 1) + minCeil);
+    console.log("computer roll result: " + roll)
+
+    let result = "";
     // turns roll into a string to be stored in result
     if (roll === 1) {
-        result = rock
+        result = rock;
     } else if (roll === 2) {
-        result = paper
+        result = paper;
     } else if (roll === 3) {
-        result = scissors
+        result = scissors;
     }
 
-    return result
+    return result;
 }
 
-function getHumanChoice(input) {
+function getHumanChoice() {
     // returns one valid choice based on user input
-    let input = prompt("Rock, paper, or scissors: ")
-    let result = ""
+    let input = prompt("Rock, paper, scissors: ");
+    let result = "";
 
     // converts input to lower-case and tests against valid choices
     if (input.toLowerCase() == rock) {
-        result = rock
+        result = rock;
     } else if (input.toLowerCase() == paper) {
-        result = paper
+        result = paper;
     } else if (input.toLowerCase() == scissors) {
-        result = scissors
+        result = scissors;
     }
 
-    return result
+    return result;
 }
 
 function playGame() {
-    let humanScore = 0
-    let computerScore = 0
+    // initializes score at 0
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        // determines the winner by testing humanChoice against computerChoice
+        // adjusts score for post-round display
+        if (humanChoice == rock) {
+            if (computerChoice == rock) {
+                console.log("It's a draw.");
+            } else if (computerChoice == paper) {
+                console.log("Computer wins.");
+                computerScore = ++computerScore;
+            } else if (computerChoice == scissors) {
+                console.log("Human wins.");
+                humanScore = ++humanScore;
+            }
+        } else if (humanChoice == paper) {
+            if (computerChoice == rock) {
+                console.log("Human wins.");
+                humanScore = ++humanScore;
+            } else if (computerChoice == paper) {
+                console.log("It's a draw.");
+            } else if (computerChoice == scissors) {
+                console.log("Computer wins.");
+                computerScore = ++computerScore;
+            }
+        } else if (humanChoice == scissors) {
+            if (computerChoice == rock) {
+                console.log("Computer wins.");
+                computerScore = ++computerScore;
+            } else if (computerChoice == paper) {
+                console.log("Human wins.");
+                humanScore = ++humanScore;
+            } else if (computerChoice == scissors) {
+                print("It's a draw.")
+            }
+        }
+        // displays post-round score
+        console.log("Human: " + humanScore)
+        console.log("Computer: " + computerScore)
+    }
+
+    playRound(getHumanChoice(), getComputerChoice(1, 3))
+    playRound(getHumanChoice(), getComputerChoice(1, 3))
+    playRound(getHumanChoice(), getComputerChoice(1, 3))
+    playRound(getHumanChoice(), getComputerChoice(1, 3))
+    playRound(getHumanChoice(), getComputerChoice(1, 3))
 }
+
+playGame()
